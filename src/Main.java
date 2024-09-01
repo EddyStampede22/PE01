8baba619555ff221bc11ae39b5fb56afadf1d819
EddyStampede22 <pimbert.chabacano@gmail.com>
2024-08-31 20:16:09 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,6 @@ public class Main {
         int total = 0;
         int counter = 0;
         roman=roman.toUpperCase();
-
         for (int i = 0; i < roman.length() - 1; i++) {
             char currentSymbol = roman.charAt(i);
             String nuevocurrent= String.valueOf(currentSymbol);
@@ -27,7 +26,7 @@ public class Main {
             if (currentValue==nextValue){
                 counter++;
                 total += currentValue;
-                if(nuevocurrent.equals("V") || nuevocurrent.equals("L") || nuevocurrent.equals("D")){
+                if(nuevocurrent.equals("V") || nuevocurrent.equals("L") || nuevocurrent.equals("D")){//Las letras V,L y D no se pueden repetir más de dos veces.
                     counter = 0;
                     total=0;
                     return total;
@@ -35,26 +34,33 @@ public class Main {
                 if (counter>2){
                     counter = 0;
                     total=0;
-                    return total;
+                    return total; // regresa cero si ya paso el limite de 3 veces repetido.
                 }
             }
+                 // filtro donde el valor actual es menor al valor que le sigue
             else if (currentValue < nextValue) {
-                if (((counter>=1) && currentValue <romanValues.get(roman.charAt(i - 1))) || counter==0  ) {
+                if (((counter>=1) && currentValue <romanValues.get(roman.charAt(i - 1))) || counter==0 ) {
                     String hola="Hola soy un filtro especial ";
                 }else {
                     total =0;
                     return total;}
-                if(nextValue == romanValues.get(roman.charAt(i - 1))) {
-                    if (nextValue==10 && currentValue==1){
-                        total -=currentValue;
-                    } else if (nextValue==100 && currentValue==10) {
-                        total -=currentValue;
+                if(i>0){
+                    //Filtros donde si los valores precedentes concuerdan con la regla.
+                    //I precede a V y a X
+                    //X precede a L y a C
+                    // C precede a D y a M
+                    if(nextValue == romanValues.get(roman.charAt(i - 1))) {
+                        if (nextValue==10 && currentValue==1){
+                            total -=currentValue;
+                        } else if (nextValue==100 && currentValue==10) {
+                            total -=currentValue;
 
-                    }else if (nextValue==1000 && currentValue==100) {
-                        total -=currentValue;
-                    }else {
-                        total=0;
-                        return total;
+                        }else if (nextValue==1000 && currentValue==100) {
+                            total -=currentValue;
+                        }else { // si  no pasa los filtros se regresa a 0
+                            total=0;
+                            return total;
+                        }
                     }
                 }
                 else if (currentValue==1 && (nextValue==5 || nextValue==10)) {
@@ -65,10 +71,11 @@ public class Main {
                     total -= currentValue;
                 }
                 else {
-                    total=0;
                     return total;
                 }
             } else {
+                //filtro para el valor que sigue es igual con el ultimo valor del numero romano.
+                //siempre y cuando el valor actual sea mayor al valor que le sigue y i es mayor 0
                 if(i>0 && nextValue==romanValues.get(roman.charAt(i - 1))) {
                     total=0;
                     return total;
@@ -83,7 +90,7 @@ public class Main {
         if (args.length>0) {
             try (PrintWriter writer=new PrintWriter(new FileWriter(filename))) {
                 writer.println("Numero Romano,Numero Decimal");//cabecera
-                for(String s : args) {
+                for(String s : args) { // se agregan los valores en sus respectivas columnas
                     if(romanToDecimal(s)!=0) {
                         writer.println(s+","+romanToDecimal(s));
                     }
